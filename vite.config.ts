14a228c85bcfdfa5319e7f592ca2e86f069d9bba
Krishna Singh <krishpintu@gmail.com>
2026-01-19 23:37:56 +0530

    Configure base path and asset inclusion in Vite
    Set base path for GitHub deployment and include Excel files as assets.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/netguard-pro/', //for github deploy  base: '/REPO_NAME/'
   assetsInclude: ["**/*.xlsx"], // <-- add this line
 });
